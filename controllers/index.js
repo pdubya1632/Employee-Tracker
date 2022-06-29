6e82d8e27bd93dab1e7bd72dc1b4842daf4662d3
Patrick Richardson Wright <wrightmight@gmail.com>
2022-06-29 09:43:56 -0700

    add all create functions to controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,28 +1,51 @@
 const { Employee, Department, Role } = require("../db/models");
 
-const listEmployees = async (req, res) => {
-    try {
-        const employees = await Employee.findAll({});
-        return res.status(201).json({
-            employees,
-        });
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-}
+// const listEmployees = async (req, res) => {
+//   try {
+//     const employees = await Employee.findAll({});
+//     return res.status(201).json({
+//       employees,
+//     });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// };
 
-const createUser = async (req, res) => {
+const createEmployee = async (req, res) => {
+  try {
+    const newEmployee = await Employee.create(req.body);
+    return res.status(201).json({
+        newEmployee,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const createRole = async (req, res) => {
     try {
-        const user = await Employee.create(req.body);
-        return res.status(201).json({
-            user,
-        });
+      const newRole = await Role.create(req.body);
+      return res.status(201).json({
+        newRole,
+      });
     } catch (error) {
-        return res.status(500).json({ error: error.message })
+      return res.status(500).json({ error: error.message });
     }
-}
+  };
+
+  const createDepartment = async (req, res) => {
+    try {
+      const newDepartment = await Department.create(req.body);
+      return res.status(201).json({
+        newDepartment,
+      });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  };
 
 module.exports = {
-    createUser,
-    listEmployees
-}
+  createEmployee,
+  createRole,
+  createDepartment
+};
