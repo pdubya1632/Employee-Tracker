98cd371245a7cbae23bfb66bd4bc62b4ad28a9ad
Patrick Richardson Wright <wrightmight@gmail.com>
2022-06-29 09:59:29 -0700

    add update employee function

--- controllers/index.js
+++ controllers/index.js
@@ -15,37 +15,54 @@ const createEmployee = async (req, res) => {
   try {
     const newEmployee = await Employee.create(req.body);
     return res.status(201).json({
-        newEmployee,
+      newEmployee,
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 };
 
-const createRole = async (req, res) => {
-    try {
-      const newRole = await Role.create(req.body);
-      return res.status(201).json({
-        newRole,
-      });
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
+const updateEmployee = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [updated] = await User.update(req.body, {
+      where: { id: id },
+    });
+    if (updated) {
+      const updatedEmployee = await User.findOne({ where: { id: id } });
+      return res.status(200).json({ employee: updatedEmployee });
     }
-  };
+    throw new Error("Employee not found");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
 
-  const createDepartment = async (req, res) => {
-    try {
-      const newDepartment = await Department.create(req.body);
-      return res.status(201).json({
-        newDepartment,
-      });
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
-    }
-  };
+const createRole = async (req, res) => {
+  try {
+    const newRole = await Role.create(req.body);
+    return res.status(201).json({
+      newRole,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const createDepartment = async (req, res) => {
+  try {
+    const newDepartment = await Department.create(req.body);
+    return res.status(201).json({
+      newDepartment,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
   createEmployee,
+  updateEmployee,
   createRole,
-  createDepartment
+  createDepartment,
 };
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const { Router } = require('express');
 const controllers = require('../controllers');
 const router = Router();
 
-router.get('/', (req, res) => res.send('This is root!'));
+router.get('/', (req, res) => res.send('You, dear user, have reached the root.'));
 
 // router.post('/employees', controllers.createUser)s;
-router.get('/employees', controllers.listEmployees);
+router.post('/employees', controllers.createEmployee);
+router.put('/employees/:id', controllers.updateEmployee);
+router.post('/roles', controllers.createRole);
+router.post('/departments', controllers.createDepartment);
 
 module.exports = router;
